21d4bd9d4c7623a1f46b77cfd6361fc9ccccef3a
warhalday <74340208+warhalday@users.noreply.github.com>
2021-02-28 13:02:03 -0600

    initial commit

---
+++ wk6/6.2/script.js
@@ -0,0 +1,15 @@
+// new instance of the Vue class
+new Vue({
+    el: "#myApp1",
+    data: {
+      songTitle: "Doo Right",
+    }
+  });
+  
+  new Vue({
+    el: "#myApp2",
+    data: {
+      artist: "by MAN MAN",
+      lyrics: "I wanna hold you Till the mountains turn into sand. Oh my God! What happened to all those days Before we were bruised. Before we were cut loose To drown in your wake. I can't breathe underwater. Like I used to 'Fore I met you. I, just can't remember January, straight through December. My collective memories are in shambles And so are my scruples. I'm outside your window Throwin' bricks at the moon. I can't breathe underwater. Like I used to 'Fore I met you"
+    }
+  });
---
+++ wk6/6A/llama.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#facts',
+    data: {
+      fact1: '1. Llamas have 2 toe nails per foot',
+      fact2: '2. It is estimated that 100 people had the surname Llama in 2021',
+      fact3: '3. Llamas are excellent security guards',
+      fact4: '4. Llamas hum',
+      fact5: '5. The average llama can run faster than Usain Bolt in his prime',
+      fact6: '6. David Spade portrayed a llama in the family comedy The Emperor\'s New Groove',
+      fact7: '7. Llama is spanish for call',
+      fact8: '8. Llamas are vegetarians and don\'t want your leftovers',
+      fact9: '9. Llama was a mid 90\'s rock band from Nashville',
+      fact10: '10. Llamas can not be considered ambidextrous as they do not have hands.',
+    }
+  })
+  
